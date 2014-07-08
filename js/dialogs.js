7b2e48c30404e74e8a3d2446da5995b85eaad3d2
labaks <las93@bk.ru>
2014-07-08 15:09:51 +0300

    added style.css. Created dialog

--- js/dialogs.js
+++ js/dialogs.js
@@ -1,6 +1,29 @@
 function dialog() {
-	var wrapper = document.getElementById('wrapper');
-	div = document.createElement("div");
-	div.innerHTML = "text";
-	wrapper.appendChild(div);
+	var wrapper = document.getElementById("wrapper");
+
+	dialogArea = document.createElement("div");
+	dialogArea.id = "dialog";
+	wrapper.appendChild(dialogArea);
+
+	dialogTitle = document.createElement("p");
+	dialogTitle.id = "dialogTitle";
+	dialogArea.appendChild(dialogTitle);
+	dialogTitle.innerHTML = "Dialog title";
+
+	butOk = document.createElement("button");
+	butOk.id = "butOk"
+	dialogArea.appendChild(butOk);
+	butOk.innerHTML = "Ok";
+
+	butCancel = document.createElement("button");
+	butCancel.id = "butCancel";
+	dialogArea.appendChild(butCancel);
+	butCancel.innerHTML = "Cancel";
+
+	butOk.onclick = function() {
+		wrapper.removeChild(document.getElementById("dialog"));
+	};
+	butCancel.onclick = function() {
+		wrapper.removeChild(document.getElementById("dialog"));
+	}
 }
